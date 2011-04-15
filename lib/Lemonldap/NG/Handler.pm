2df926d9f0c51f3f6fa90b4cc50e1f50dcd6a548
Clément OUDOT <clem.oudot+cpan@gmail.com>
2011-04-15 14:51:16 +0000

    Import of COUDOT/Lemonldap-NG-Handler-1.0.5 from CPAN.
    gitpan-cpan-distribution: Lemonldap-NG-Handler
    gitpan-cpan-version:      1.0.5
    gitpan-cpan-path:         COUDOT/Lemonldap-NG-Handler-1.0.5.tar.gz
    gitpan-cpan-author:       COUDOT
    gitpan-cpan-maturity:     released

--- lib/Lemonldap/NG/Handler.pm
+++ lib/Lemonldap/NG/Handler.pm
@@ -5,7 +5,7 @@
 # Alias for Lemonldap::NG::Handler::SharedConf
 package Lemonldap::NG::Handler;
 
-our $VERSION = '1.0.4';
+our $VERSION = '1.0.5';
 use Lemonldap::NG::Handler::SharedConf;
 use base qw(Lemonldap::NG::Handler::SharedConf);
 
